8535c1cd5b03a62cd1c5b68c48c67b348fbb0a55
Roy Blankman <riblankman@gmail.com>
2017-11-07 10:56:48 -0500

    more progress

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # ethereumhs-util
 
+A collection of utility functions for ethereum inspired by
+[ethereumjs-util](https://github.com/ethereumjs/ethereumjs-util).
+
 ## build deps for secp256k1
 
 - automake
--- src/Network/Ethereum/Util.hs
+++ src/Network/Ethereum/Util.hs
@@ -7,6 +7,7 @@ import           Crypto.Secp256k1
 import           Data.ByteString.Short (ShortByteString, fromShort, toShort)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Base16 as BS16
+import           Data.Either.Utils (maybeToEither)
 import           Data.Maybe (listToMaybe)
 import           Data.Text (Text)
 import qualified Data.Text as T
@@ -30,12 +31,7 @@ decomposeSig sig
           adjustV x | x < 27    = x
                     | otherwise = x - 27
 
--- TODO find a library that implements this
-maybeToEither :: b -> Maybe a -> Either b a
-maybeToEither msg (Just a) = Right a
-maybeToEither msg Nothing = Left msg
-
-ecrecover :: Text -> Text -> Either String (PubKey, Text, Text, Text, Text)
+ecrecover :: Text -> Text -> Either String (PubKey, Text, Text, Text, Text, Text)
 ecrecover sig message = do
     compactRecSig <- maybeToEither "decompose sig error" $ decomposeSig sig
     recSig <- maybeToEither "importCompactRecSig error" $ importCompactRecSig compactRecSig
@@ -47,18 +43,37 @@ ecrecover sig message = do
     -- take last 160 bits of pubkey
     let ethAddr = T.drop 24 keyHash
     let ethAddr2 = T.drop 24 keyHash2
-    return (pubkey, keyHash, ethAddr, keyHash2, ethAddr2)
+    return (pubkey, T.decodeUtf8 . BS16.encode $ exportPubKey False pubkey, keyHash, ethAddr, keyHash2, ethAddr2)
 
+-- pubToAddress
+{-
+    -
+/**
+ * Returns the ethereum address of a given public key.
+ * Accepts "Ethereum public keys" and SEC1 encoded keys.
+ * @param {Buffer} pubKey The two points of an uncompressed key, unless sanitize is enabled
+ * @param {Boolean} [sanitize=false] Accept public keys in other formats
+ * @return {Buffer}
+ */
+exports.pubToAddress = exports.publicToAddress = function (pubKey, sanitize) {
+  pubKey = exports.toBuffer(pubKey)
+  if (sanitize && (pubKey.length !== 64)) {
+    pubKey = secp256k1.publicKeyConvert(pubKey, false).slice(1)
+  }
+  assert(pubKey.length === 64)
+  // Only take the lower 160bits of the hash
+  return exports.sha3(pubKey).slice(-20)
+}
+-}
 
 hashPersonalMessage :: Text -> Text
-hashPersonalMessage m = T.pack . show $ (hash concatenatedString :: Digest Keccak_256)
-    where messageBytes = bytesDecode m
-          prefixString = "\EMEthereum Signed Message:\n" ++ (show $ B.length messageBytes)
-          prefix = T.encodeUtf8 . T.pack $ prefixString
-          concatenatedString = prefix `B.append` messageBytes
-
+hashPersonalMessage message = T.pack . show $ keccakDigest
+    where messageBytes = bytesDecode message
+          prefix = T.encodeUtf8 . T.pack $ "\EMEthereum Signed Message:\n" ++ (show $ B.length messageBytes)
+          keccakDigest :: Digest Keccak_256
+          keccakDigest = hash (prefix `B.append` messageBytes)
 
 test = ecrecover sigT $ hashPersonalMessage messageT
     where sigT = "819df6d812858e093b28f001e5d85527cf72dcc2c5ba478bb78ca73ef96449f92f0865223bb54e0b8d7fdcccc0e4cc9bb63cb65259502d7f6c6fbcfb82cb485b1c"
           messageT = "8db36fe7023731c87ba645cab36ea211f224fe1dc38f27d0708c5d6218f3a492"
-          pmessageT = "49de9e6a08cc856ae51c8b78358756379f75079edcbca21133c32d88d4075d4f"
+          -- pmessageT = "49de9e6a08cc856ae51c8b78358756379f75079edcbca21133c32d88d4075d4f"
